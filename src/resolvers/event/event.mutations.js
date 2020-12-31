20e84660aef798f4ccfcaabc0127317c6ef362a7
Ivan Naluzhnyi <ivan.naluzhnyi@gmail.com>
2020-12-31 01:10:59 +0100

    update event subscription

--- src/resolvers/event/event.mutations.js
+++ src/resolvers/event/event.mutations.js
@@ -1,28 +1,24 @@
-const { forwardTo } = require("prisma-binding");
-
-const {
-    publishUpdate,
-    publishDelete,
-    publishCreate,
-} = require("./event.subscription");
+const { publishEventByType } = require("./event.subscription");
+const { SubscriptionActionTypes } = require("../../constants/enums");
 
 async function createEvent(parent, args, ctx, info) {
     const createdEvent = await ctx.prisma.mutation.createEvent(args);
-    await publishCreate(ctx, createdEvent);
+    await publishEventByType(ctx, createdEvent, SubscriptionActionTypes.CREATE);
     return createdEvent;
 }
 
 async function updateEvent(parent, args, ctx, info) {
-    delete args.where.owenerId;
-    const response = await forwardTo("prisma")(parent, args, ctx, info);
-    await publishUpdate(ctx, args);
-    return response;
+    const updatedEvent = await ctx.prisma.mutation.updateEvent(args);
+    await publishEventByType(ctx, updatedEvent, SubscriptionActionTypes.UPDATE);
+
+    return updatedEvent;
 }
 
 async function deleteEvent(parent, args, ctx, info) {
-    await publishDelete(ctx, args);
+    const deletedEvent = await ctx.prisma.mutation.deleteEvent(args);
+    await publishEventByType(ctx, deletedEvent, SubscriptionActionTypes.DELETE);
 
-    return forwardTo("prisma")(parent, args, ctx, info);
+    return deletedEvent;
 }
 
 module.exports = {
--- src/resolvers/event/event.subscription.js
+++ src/resolvers/event/event.subscription.js
@@ -1,47 +1,23 @@
 const { PUPSUB_EVENT } = require("../../constants/subscriptions.types");
 const { SubscriptionActionTypes } = require("../../constants/enums");
 
-const publishCreate = async (ctx, createdEvent) => {
+/**
+ *
+ * @param {*} ctx
+ * @param {*} event
+ * @param {SubscriptionActionTypes} actionType
+ */
+const publishEventByType = async (ctx, event, actionType) => {
     const { pubsub } = ctx;
 
     pubsub.publish(PUPSUB_EVENT, {
         event: {
-            actionType: SubscriptionActionTypes.CREATE,
-            event: createdEvent,
-        },
-    });
-};
-
-const publishUpdate = async (ctx, args) => {
-    const { prisma, pubsub } = ctx;
-    const updatedEvent = await prisma.query.event({
-        where: { id: args.where.id },
-    });
-
-    pubsub.publish(PUPSUB_EVENT, {
-        event: {
-            actionType: SubscriptionActionTypes.UPDATE,
-            event: updatedEvent,
-        },
-    });
-};
-
-const publishDelete = async (ctx, args) => {
-    const { prisma, pubsub } = ctx;
-    const deletedEvent = await prisma.query.event({
-        where: { id: args.where.id },
-    });
-
-    pubsub.publish(PUPSUB_EVENT, {
-        event: {
-            actionType: SubscriptionActionTypes.DELETE,
-            event: deletedEvent,
+            actionType,
+            event,
         },
     });
 };
 
 module.exports = {
-    publishUpdate,
-    publishDelete,
-    publishCreate,
+    publishEventByType,
 };
